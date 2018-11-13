da9cd0f4791c732390e320bca5ecc1c740822dac
Joe LaRue <jlarue617@gmail.com>
2018-11-13 18:38:29 -0500

    added student list component.  added bootstrap

---
+++ src/app/model.ts
@@ -0,0 +1,27 @@
+export class User{
+    id: number;
+    userName: string;
+    creationDate: Date;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    lastLogon?: Date;
+    roleId: number;    
+}
+
+export class Role{
+    id: number;
+    description: string;
+}
+
+export class Technique{
+    id: number;
+    description: string;
+    techniqueType: TechniqueType;
+}
+
+export class TechniqueType{
+    id: number;
+    description: string;
+}
---
+++ src/app/student-list.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+import { User } from './model';
+
+@Component({
+  selector: 'student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent  {
+
+  users: User[] = [
+    {
+      id: 1,
+      creationDate: new Date('1/1/2018'),
+      email: 'joe@email.com',
+      firstName: 'Joe',
+      lastName: 'LaRue',
+      roleId: 1,
+      userName: 'jlarue',
+      password: 'password'  
+    },
+    {
+      id: 2,
+      creationDate: new Date('1/1/2018'),
+      email: 'colleen@email.com',
+      firstName: 'Colleen',
+      lastName: 'LaRue',
+      roleId: 1,
+      userName: 'clarue',
+      password: 'password'  
+    },
+    {
+      id: 3,
+      creationDate: new Date('1/1/2018'),
+      email: 'jj@email.com',
+      firstName: 'JJ',
+      lastName: 'LaRue',
+      roleId: 1,
+      userName: 'jjlarue',
+      password: 'password'  
+    },
+    {
+      id: 1,
+      creationDate: new Date('1/1/2018'),
+      email: 'jenna@email.com',
+      firstName: 'jenna',
+      lastName: 'LaRue',
+      roleId: 1,
+      userName: 'jennalarue',
+      password: 'password'  
+    }
+  ]
+
+}
